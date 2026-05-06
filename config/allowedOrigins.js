34b7fbce6f6fa3647ed0689139c8e0c69fb374dc
Cole Brown <c_brown23@mail.fhsu.edu>
2026-05-05 22:20:42 -0500

    allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,4 +1,5 @@
 const allowedOrigins = [
+    'https://cole-brown-final.onrender.com',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'https://dazzling-snickerdoodle-777101.netlify.app'
