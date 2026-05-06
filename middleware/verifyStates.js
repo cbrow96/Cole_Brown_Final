ea84f527944e015b30194e2ee97afb2702dac430
Cole Brown <c_brown23@mail.fhsu.edu>
2026-05-05 23:37:03 -0500

    verify

--- middleware/verifyStates.js
+++ middleware/verifyStates.js
@@ -1,10 +1,10 @@
 const statesData = require('../model/statesData.json');
 
 const verifyStates = (req, res, next) => {
-    const stateCode = req.params.statesData.toUpperCase();
+    const stateCode = req.params.state.toUpperCase();
 
     const codeCheck = statesData.find(s => s.code === stateCode);
-    if (!validState) {
+    if (!codeCheck) {
         return res.status(400).json({ message: 'Invalid state abbreviation parameter' });
     }
 
